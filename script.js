2502f34bcc566ede2b5507e1c93df39e6018ee4c
takumifukasawa <fukapon912@gmail.com>
2015-04-28 20:41:51 +0900

    youtube.jsの外部化

--- script.js
+++ script.js
@@ -1,99 +1,10 @@
 (function(win, doc, $){
 
-    var player;
-    var videoID = "SnXkhkEvNIM";
+    "use strict";
 
-    var script = document.createElement('script');
-    script.src = 'https://www.youtube.com/iframe_api';
-    document.body.appendChild(script);
-
-    // playerの準備完了時
-    win.onYouTubeIframeAPIReady = function() {
-        console.log("onYouTubeIframeAPIReady");
-        loadPlayer(videoID);
-    };
-
-    // playerの生成
-    function loadPlayer(videoID) {
-        console.log("loadPlayer(" + videoID + ")");
-        if(!player) {
-            player = new YT.Player('player', {
-                    width: '640',
-                    height: '390',
-                    videoId: videoID,
-                    events: {
-                        "onReady": onPlayerReady,
-                        "onStateChange": onPlayerStateChange,
-                    },
-                    playerVals: {
-                        "rel": 0,
-                        "showInfo": 0,
-                        "controls": 1
-                    }
-                }
-            );
-        } else {
-            player.loadVideoByID(videoID);
-        }
-    }
-
-    // player の準備完了時
-    function onPlayerReady(event) {
-        console.log("onPlayerReady");
-    }
-
-    // playerのstatusが変更される度に発生
-    function onPlayerStateChange(event) {
-        console.log("PlayerState:" + event.data);
-        switch(event.data) {
-            case YT.PlayerState.ENDED:
-            case YT.PlayerState.PAUSED:
-            case YT.PlayerState.CUED:
-                $("#play").html("再生");
-                break;
-            case YT.PlayerState.PLAYING:
-            case YT.PlayerState.BUFFERING:
-                $("#play").html("一時停止");
-                break;
-            default:
-                $("#play").html("再生");
-                break;
-        }
-
-    }
-
-    function play() {
-        console.log("play");
-        player.playVideo();
-        $(this).html("再生");
-    }
-    function pause() {
-        console.log("pause");
-        player.pauseVideo();
-        $(this).html("一時停止");
-    }
-    function stop() {
-        console.log("stop");
-        player.stopVideo();
-        player.cueVideoById(videoID);
-        $(this).html("一時停止");
-    }
-
-    $("#play").click(function() {
-        var label = $(this).html();
-        console.log("play");
-        if(label == "再生") {
-            play();
-        } else {
-            pause();
-        }
-    });
-    $("#stop").click(function() {
-        stop();
-    });
-    $("#seek").click(function() {
-        player.seekTo(30, true);
-        play();
+    var youtube = new Youtube({
+            videoID: "SnXkhkEvNIM",
     });
 
-})(window, document, $);
+})(window, document, jQuery);
+
---
+++ youtube.js
@@ -0,0 +1,104 @@
+var Youtube = function(opt) {
+    var opts = opts || {};
+    this.videoID = opts.videoID;
+    //this.url = opts.url || {};
+    this.player = opts.player || null;
+
+    this.initalize();
+};
+
+Youtube.prototype.initialize = function() {
+    // youtube読み込み
+    var script = document.createElement('script');
+    script.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(script);
+    // playerの準備完了時
+    window.onYouTubeIframeAPIReady = function() {
+        console.log("onYouTubeIframeAPIReady");
+        loadPlayer(this.videoID);
+    };
+};
+
+// youtube player をロードする
+Youtube.prototype.loadPlayer = function() {
+    console.log("loadPlayer(" + this.videoID + ")");
+    if(!this.player) {
+        this.player = new YT.Player('player', {
+                        width: '640',
+                        height: '390',
+                        videoId: this.videoID,
+                        events: {
+                            "onReady": this.onPlayerReady,
+                            "onStateChange": this.onPlayerStateChange,
+                        },
+                        playerVals: {
+                            "rel": 0,
+                            "showInfo": 0,
+                            "controls": 0
+                        }
+        });
+    } else {
+        this.player.loadVideoByID(videoID);
+    }
+};
+
+// player の準備完了時
+Youtube.prototype.onPlayerReady = function() {
+    console.log("onPlayerReady");
+};
+
+// playerのstatusが変更される度に発生
+Youtube.prototype.onPlayerStateChange = function(event) {
+    console.log("PlayerState:" + event.data);
+    switch(event.data) {
+        case YT.PlayerState.ENDED:
+        case YT.PlayerState.PAUSED:
+        case YT.PlayerState.CUED:
+            break;
+        case YT.PlayerState.PLAYING:
+        case YT.PlayerState.BUFFERING:
+            $("#play").html("一時停止");
+            break;
+        default:
+            $("#play").html("再生");
+            break;
+    }
+};
+
+/*
+    var player;
+    var videoID = "SnXkhkEvNIM";
+     function play() {
+        console.log("play");
+        player.playVideo();
+        $(this).html("再生");
+    }
+    function pause() {
+        console.log("pause");
+        player.pauseVideo();
+        $(this).html("一時停止");
+    }
+    function stop() {
+        console.log("stop");
+        player.stopVideo();
+        player.cueVideoById(videoID);
+        $(this).html("一時停止");
+    }
+
+    $("#play").click(function() {
+        var label = $(this).html();
+        console.log("play");
+        if(label == "再生") {
+            play();
+        } else {
+            pause();
+        }
+    });
+    $("#stop").click(function() {
+        stop();
+    });
+    $("#seek").click(function() {
+        player.seekTo(30, true);
+        play();
+    });
+    */
